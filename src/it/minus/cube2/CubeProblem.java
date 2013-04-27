4476b7782e72f2deaf87d08a51593d59c2bd898e
Mario Minutolo <mario.minutolo@gmail.com>
2013-04-27 15:48:09 +0200

    Problem skeleton

--- src/it/minus/cube2/CubeProblem.java
+++ src/it/minus/cube2/CubeProblem.java
@@ -1,5 +1,21 @@
 package it.minus.cube2;
 
 public class CubeProblem {
+	private long initialState = 0;
 
+	public CubeProblem() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CubeProblem(long initialState) {
+		super();
+		this.initialState = initialState;
+	}
+
+	public long getInitialState() {
+		return initialState;
+	}
+	
+	
 }
